2db7d08a4ec0a82fd74ce3789c40ffeb52869aaa
Anykan <aq-22@web.de>
2016-10-06 00:45:57 +0200

    ..

--- dag.h
+++ dag.h
@@ -1,96 +1,8 @@
-//Einbindung von Standard-Bibliotheken
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include <string.h>
-#include <sys/types.h>
-
-//Einbindung eigener, lokaler Bibliotheken
-#include "conv.h"
-#define MAX 100000 // Unendlich
-
-//Definition eigener Funktionen
 int TopSort(int anzahlKnoten, int ** AdjMat, int ** Kantengewicht, int * Eingangsgrad, int * TopologischeSortierung);
 int DAG ( int anzahlKnoten, int ** AdjMat, int ** Kantengewicht, int * Eingangsgrad, int startKnoten, int zielKnoten, int * Vorgaenger, int * WeglaengeZuKnoten, int printDetails );
 int ShowShortestPath ( int anzahlKnoten, int ** AdjMat, int startKnoten, int zielKnoten, int * Vorgaenger, int * WeglaengeZuKnoten );
 
-//Definition der main-Funktion
-int main (int  argc, char ** argv )
-{
-	system("cls");
-	//Überprüfe Argumente
-	if ( 3 != argc ) 
-	{
-		printf("\nProgrammaufruf: DAG.exe Graph.txt startKnoten\n"); 
-		return 1; 
-	}
 
-	int i;
-
-	//Einlesen aus Datei
-	FILE * file_Graph = fopen( argv[ 1 ], "r" );
-	if( file_Graph == 0 )
-	{
-		printf("\n Problem: Datei %s konnte nicht geoeffnet werden.\n", argv[ 1 ] );
-		return 1;
-	}	
-	
-	int anzahlKnoten, anzahlKanten;
-	fscanf( file_Graph, "%d", &anzahlKnoten );
-	fscanf( file_Graph, "%d", &anzahlKanten );
-
-	int ** AdjMat = GenerateIntMat( anzahlKnoten+1, anzahlKnoten+1 );
-	int ** Kantengewicht = GenerateIntMat( anzahlKnoten + 1, anzahlKnoten + 1 );
-	int * Eingangsgrad = GenerateIntVect( anzahlKnoten+1 );
-
-	int von, zu, gewicht;
-	
-	
-	for( i = 1; i <= anzahlKanten; i++ ) 
-	{
-		fscanf( file_Graph, "%d %d %d", &von, &zu, &gewicht );
-		Kantengewicht[von][zu]= gewicht;
-		AdjMat[von][0]++;
-		Eingangsgrad[zu]++;  // Wozu eingangsgrad wenn spalte 0 dafür schon genutz wird ????
-		AdjMat[von][AdjMat[von][0]]=zu;
-	}	
-	fclose( file_Graph );
-//	ShowIntMat ( 1, anzahlKnoten, 0, anzahlKnoten, AdjMat, "Vorgaenger" );
-//	ShowIntMat ( 1, anzahlKnoten, 1, anzahlKnoten, Kantengewicht, "Kantengewicht" );
-//	ShowIntVect( 1, anzahlKnoten, Eingangsgrad, "Eingangsgrad" );
-	
-//DAG
-	int startKnoten = atoi ( argv[ 2 ] );
-	int * WeglaengeZuKnoten = GenerateIntVect ( anzahlKnoten + 1 ); //entspricht Vektor C im Quellcode auf Vorlesungsfolien
-	int * Vorgaenger = GenerateIntVect ( anzahlKnoten+1 );	//entspricht Vektor v im Quellcode auf Vorlesungsfolien
-
-	DAG ( anzahlKnoten, AdjMat, Kantengewicht, Eingangsgrad, startKnoten, -1, Vorgaenger, WeglaengeZuKnoten, 1 );
-
-	for (i = 1; i <= anzahlKnoten; i++)
-	{
-		if (i != startKnoten)
-		{
-			ShowShortestPath ( anzahlKnoten, AdjMat, startKnoten, i, Vorgaenger, WeglaengeZuKnoten );
-		}
-	}
-
-
-	//4. HIER: KÜRZESTE WEGE IN DATEI SPEICHERN
-
-
-
-	//Speicherfreigabe	
-	if (Vorgaenger != NULL) free ( Vorgaenger );
-	if (WeglaengeZuKnoten != NULL) free ( WeglaengeZuKnoten );
-	if (Eingangsgrad != NULL) free ( Eingangsgrad );
-	FreeIntMat( anzahlKnoten, AdjMat );
-	FreeIntMat( anzahlKnoten, Kantengewicht );
-
-	return 0; 
-}
-
-//2. TOP-SORT IMPLEMENTIEREN, ERGEBNIS SOLL IN VEKTOR TopologischeSortierung STEHEN
 int TopSort ( int anzahlKnoten, int ** AdjMat, int ** Kantengewicht, int * Eingangsgrad, int * TopologischeSortierung )
 {
 	int i, j;
@@ -120,7 +32,7 @@ int TopSort ( int anzahlKnoten, int ** AdjMat, int ** Kantengewicht, int * Einga
 	
 	return 0;
 }
-//3. HIER DAG-ALGORITHMUS IMPLEMENTIEREN
+
 int DAG ( int anzahlKnoten, int ** AdjMat, int ** Kantengewicht, int * Eingangsgrad, int startKnoten, int zielKnoten, int * Vorgaenger, int * WeglaengeZuKnoten, int printDetails )
 {
 	int i, j, k;
@@ -129,7 +41,7 @@ int DAG ( int anzahlKnoten, int ** AdjMat, int ** Kantengewicht, int * Eingangsg
 	TopologischeSortierung = GenerateIntVect ( anzahlKnoten+1 );
 
 	TopSort ( anzahlKnoten, AdjMat, Kantengewicht, Eingangsgrad, TopologischeSortierung );
-//	ShowIntVect ( 1, anzahlKnoten, TopologischeSortierung, "TopologischeSortierung" );	
+	ShowIntVect ( 1, 30, TopologischeSortierung, "TopologischeSortierung" );	
 	
 	for (j=1;j<=anzahlKnoten;j++){
 		WeglaengeZuKnoten[j]=MAX;
@@ -188,7 +100,7 @@ int ShowShortestPath ( int anzahlKnoten, int ** AdjMat, int startKnoten, int zie
 	}
 	else
 	{
-		printf ( "\nEs exisitiert kein Weg von Startknoten %d zu Zielknoten %d!\n", startKnoten, zielKnoten );
+//		printf ( "\nEs exisitiert kein Weg von Startknoten %d zu Zielknoten %d!\n", startKnoten, zielKnoten );
 	}
 
 	return 1;
--- ffap.c
+++ ffap.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <math.h>
+
+#define MAX 100000
 #include "conv.h"
+#include "dag.h"
+
 
 struct Zuege
 {
@@ -54,21 +62,78 @@ int main(int  argc, char ** argv )
 	int ** AdjMat = GenerateIntMat( anzahlKnoten+1, anzahlKnoten+1 );
 	int ** Kantengewicht = GenerateIntMat( anzahlKnoten + 1, anzahlKnoten + 1 );
 
+// von 23 nach 1
+// min abf 90
+// max ziel 1383
+// umsteig 5
+
+
+// Typ A Kanten
+// im Startbahnhof	suche Ereignis welche von frühste start zeit aus möglich
+	for(i=1;i<=anzahlZuege;i++){
+		if(Zug[i].start==Sendung.start && Zug[i].zeitStart>Sendung.zeitStart){
+			AdjMat[(2*anzahlZuege+1)][0]++;
+			AdjMat[(2*anzahlZuege+1)][AdjMat[(2*anzahlZuege+1)][0]]=i;
+			Eingangsgrad[(2*anzahlZuege+1)]++;
+		}
+	}
+
 // Typ B Kanten
 	for(i=1;i<=anzahlZuege;i++){
 		AdjMat[i][0]=1;
 		AdjMat[i][1]=anzahlZuege+i;
+		Eingangsgrad[i]++;
 	}
-// Typ C
+// Typ C Kanten
 	for(i=1;i<=anzahlZuege;i++){
 		for(j=1;j<=anzahlZuege;j++){
-			if(Zug[i].zeitZiel+Umsteigzeit<Zug[j].zeitStart && Zug[i].ziel==Zug[j].start){  // Bahnhofüberprüfen ob start = ziel fehlt noch
-				printf("%d -> %d\n",i,j); // hioer komm dag zeile 54 ff. wenn kannte existiret in spalt 0 ++ und nachfolger eintragen
-			}
-			
+			if(Zug[i].zeitZiel+Umsteigzeit<Zug[j].zeitStart && Zug[i].ziel==Zug[j].start){
+				AdjMat[anzahlZuege+i][0]++;
+				Eingangsgrad[anzahlZuege+j]++;
+				AdjMat[anzahlZuege+i][AdjMat[anzahlZuege+i][0]]=anzahlZuege+j;
+			}	
 		}
 	}
+// Typ D Kanten 
+//im Zielbahnhof suche ereignis welche ereignis noch zeitlich ankommt
+	for(i=1;i<=anzahlZuege;i++){
+		if(Zug[i].ziel==Sendung.ziel && Zug[i].zeitZiel<Sendung.zeitZiel){
+			AdjMat[(2*anzahlZuege+2)][0]++;
+			AdjMat[(2*anzahlZuege+2)][AdjMat[(2*anzahlZuege+2)][0]]=i;
+			Eingangsgrad[(2*anzahlZuege+2)]++;
+		}
+	}
+// Kantengewicht
 
+	for(i=1;i<=AdjMat[(2*anzahlZuege+2)][0];i++){
+		Kantengewicht[(2*anzahlZuege+2)][i] = Zug[i].zeitZiel;
+	}
+
+// DAG
+
+	int startKnoten = Sendung.start;
+	int zielKnoten = Sendung.ziel;
+	int * WeglaengeZuKnoten = GenerateIntVect ( anzahlKnoten + 1 );
+	int * Vorgaenger = GenerateIntVect ( anzahlKnoten+1 );
+
+	DAG ( anzahlKnoten, AdjMat, Kantengewicht, Eingangsgrad, startKnoten, zielKnoten, Vorgaenger, WeglaengeZuKnoten, 0 );
+
+	for (i = 1; i <= anzahlKnoten; i++)
+	{
+		if (i != startKnoten)
+		{
+			ShowShortestPath ( anzahlKnoten, AdjMat, startKnoten, i, Vorgaenger, WeglaengeZuKnoten );
+		}
+	}	
+	
+	
+//	ShowIntMat ( 1, 20, 0, 15, AdjMat, "Abfahrtsereignis" );
+//	ShowIntMat ( 601, 630, 0, 20, AdjMat, "Ankunftsereignis" );
+//	ShowIntVect( 0, 1202, Eingangsgrad, "Eingangsgrad" );
+//	ShowIntMat ( 1201, 1201, 0, 20, AdjMat, "Aufkommensereignis" );	
+//	ShowIntMat ( 1202, 1202, 0, 30, AdjMat, "Zustellungsereignis" );	
+//	ShowIntMat ( 1202, 1202, 0, 30, Kantengewicht, "Kantengewicht" );
+	
 	FreeIntMat( anzahlKnoten+1, Kantengewicht );
 	FreeIntMat( anzahlKnoten+1, AdjMat );
 	free ( Eingangsgrad );
